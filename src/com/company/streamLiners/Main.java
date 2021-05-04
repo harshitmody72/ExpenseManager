a81d894f70614880e4e9ee4854ef31ea0980c03b
harshitmody72 <harshit.19bcon189@jecrcu.edu.in>
2021-05-04 20:24:22 +0530

    Initial Commit

--- src/com/company/streamLiners/Main.java
+++ src/com/company/streamLiners/Main.java
@@ -1,8 +1,13 @@
 package com.company.streamLiners;
 
+import com.company.streamLiners.Models.Transactions;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Transactions tr = new Transactions("11 28 2018",1800,0);
+        System.out.println(tr);
+
     }
 }
---
+++ src/com/company/streamLiners/Models/TransactionType.java
@@ -0,0 +1,5 @@
+package com.company.streamLiners.Models;
+
+public class TransactionType {
+    public static final int TYPE_exp = 0 , TYPE_income = 1;
+}
